59813aaed7879f3b6e63339573d648bfe469570d
TaichiNomura <24144033@ed.tmu.ac.jp>
2025-10-21 04:35:37 +0000

    Add YourScriptName.cs with self-introduction

---
+++ YourScriptName.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class YourScriptName : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("野村太一");
+        Debug.Log("24144033");
+        Debug.Log("TaichiNomura");
+    }
+}
